6dae43a38e58e00e2e8aa15243f7b8bda80bfc2c
tal shachar <tal.shachar@buzzfeed.com>
2017-12-10 01:05:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,4 +4,15 @@ def initialize(board = nil)
   @board = board || Array.new(9," ")
 end
 
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 end
